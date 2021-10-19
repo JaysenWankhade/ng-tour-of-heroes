ad7a6007b9b41b7e1cc008f75cda8b5a640c6ff1
JaysenWankhade <jaysenwankhade@hotmail.com>
2021-10-19 11:57:10 +0530

    written new code

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -56,4 +56,5 @@ export class HeroService {
 
   //added a new comment just to test branching
   //added a new comment just to test branching #1
+  //added a new comment just to test branching #2
 }
